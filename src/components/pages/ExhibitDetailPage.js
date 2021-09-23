eca4ea0f6c5ba136c20e7c47fdccb5587de64bcd
Viktor Moskalev <viktormoskalev07@gmail.com>
2021-09-23 17:10:04 +0300

    youTube to livespotting

--- src/components/pages/ExhibitDetailPage.js
+++ src/components/pages/ExhibitDetailPage.js
@@ -9,6 +9,7 @@ import { streams }  from '../../db';
 const ExhibitDetailPage = ({ match }) => {
   const i = streams.find(i => i.slug === match.params.slug);
   const { name, image, slug, streamUrl, description } = i;
+  const videoUrl = "https://player.livespotting.com?alias=0ejpnnrt&ch="+streamUrl; 
   return (
     <>
       <Container>
@@ -22,14 +23,24 @@ const ExhibitDetailPage = ({ match }) => {
           <Row>
             <Col sm="12" md="8">
               <div className="embed-responsive embed-responsive-16by9">
-                <iframe
+                {/* <iframe
                   title={`Live stream of the ${name} exhibit`}
                   className="embed-responsive-item"
                   src={streamUrl}
                   frameBorder="0"
                   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
-                />
+                /> */}
+                      <iframe src =  {videoUrl} 
+                      title={`Live stream of the ${name} exhibit`}
+                      name = "ls-player" 
+                      scrolling = "no"
+                      className="embed-responsive-item"
+                      frameBorder = "0"
+                      webkitallowfullscreen = "true"
+                      mozallowfullscreen = "true"
+                      allowFullScreen   > </iframe>
+
               </div>
             </Col>
             <Col>
@@ -67,7 +78,7 @@ const ExhibitDetailPage = ({ match }) => {
 
       <div className="hwrapper">
         <div className="hwrapper-inner">
-          {streams.map(i => <SlideCard slide={i} />)}
+          {streams.map((item ,i) => <SlideCard slide={item} key={i} />)}
         </div>
       </div>
     </>
