53417ac69ff9b1f5ab56f55efd08ca7b859ae9dd
Viktor Moskalev <viktormoskalev07@gmail.com>
2021-09-30 18:43:22 +0300

    fix8

--- src/components/pages/Frontpage.js
+++ src/components/pages/Frontpage.js
@@ -1,4 +1,5 @@
-  import { Link   } from 'react-router-dom';
+import React from 'react';
+ import { Link   } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import Overdrive from 'react-overdrive';
